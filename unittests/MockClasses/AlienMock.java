bffdd778254a7061924a28692fdee664366e20bb
Bastiaan Clement <bastiaan.clement@simac.com>
2021-01-10 12:04:58 +0100

    Restored AlienMock unit test. Added highscore unit test. Game works. Except evaluation of highscore. When user doesn't exceed the points it still will be prompted to set a new highscore.

--- unittests/MockClasses/AlienMock.java
+++ unittests/MockClasses/AlienMock.java
@@ -1,77 +1,92 @@
-//package MockClasses;
-//
-//import logic.entities.MovingGameObject;
-//import bootstrap.GamePanel;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//
-//public class AlienMock extends MovingGameObject {
-//    private int alienType;
-//    private int alienWidth;
-//    private int alienHeight;
-//
-//    private final ImageIcon alien1 = new ImageIcon("images/alien-blue.gif");
-//    private final ImageIcon alien2 = new ImageIcon("images/alien-orange.gif");
-//    private final ImageIcon alien3 = new ImageIcon("images/alien-red.gif");
-//    private final ImageIcon alienBoss = new ImageIcon("images/alien-bos-green.gif");
-//    private final ImageIcon alienBoss2 = new ImageIcon("images/alien-bos-purple.gif");
-//    private final ImageIcon alienBoss3 = new ImageIcon("images/alien-bos-red.gif");
-//
-//
-//
-//    public AlienMock(int xValue, int yValue, int velocityX, int velocityY, int alienType, Color color, int alienWidth, int alienHeight) {
-//        super(xValue, yValue, velocityX, velocityY, color);
-//        this.alienType = alienType;
-//        this.alienWidth = alienWidth;
-//        this.alienHeight = alienHeight;
-//    }
-//
-//
-//    public void draw(Graphics g) {
-//        // Varient 1
-//        if (this.alienType % 3 == 0) {
-//            alien1.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
-//            // Varient 2
-//        } else if (this.alienType % 3 == 1 && this.alienType != 100) {
-//            alien2.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
-//            // Varient 3
-//        } else if (this.alienType % 3 == 2) {
-//            alien3.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
-//            // Boss Enemy
-//        } if (this.alienType == 100)
-//        {
-//            if(GamePanel.getHealthAlienBoss() > 20){
-//                alienBoss.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
-//            }
-//            else if(GamePanel.getHealthAlienBoss() > 10){
-//                alienBoss2.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
-//            }
-//            else if(GamePanel.getHealthAlienBoss() > 0){
-//                alienBoss3.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public Rectangle getBounds(){
-//        Rectangle alienHitRectangle = new Rectangle(this.getXCoordinateValue(), this.getYCoordinateValue(), alienWidth, alienHeight);
-//        return  alienHitRectangle;
-//    }
-//
-//    @Override
-//    public void move(){
-//    }
-//
-//    public int getAlienType() {
-//        return alienType;
-//    }
-//
-//    public int getAlienWidth() {
-//        return alienWidth;
-//    }
-//
-//    public int getAlienHeight() {
-//        return alienHeight;
-//    }
-//}
+package MockClasses;
+
+import logic.entities.MovingGameObject;
+import javax.swing.*;
+import java.awt.*;
+
+public class AlienMock extends MovingGameObject {
+    private final ImageIcon alien1 = new ImageIcon("images/alien-blue.gif");
+    private final ImageIcon alien2 = new ImageIcon("images/alien-orange.gif");
+    private final ImageIcon alien3 = new ImageIcon("images/alien-red.gif");
+    private final ImageIcon alienBoss = new ImageIcon("images/alien-bos-green.gif");
+    private final ImageIcon alienBoss2 = new ImageIcon("images/alien-bos-purple.gif");
+    private final ImageIcon alienBoss3 = new ImageIcon("images/alien-bos-red.gif");
+
+    private int alienType;
+    private int alienWidth;
+    private int alienHeight;
+    private int alienHealth;
+
+    public AlienMock(int xValue, int yValue, int velocityX, int velocityY, int alienType, Color color, int alienWidth, int alienHeight) {
+        super(xValue, yValue, velocityX, velocityY, color);
+        this.alienType = alienType;
+        this.alienWidth = alienWidth;
+        this.alienHeight = alienHeight;
+        this.alienHealth = 1;
+    }
+
+    public AlienMock(int xValue, int yValue, int velocityX, int velocityY, int alienType, Color color, int alienWidth, int alienHeight, int alienHealth) {
+        super(xValue, yValue, velocityX, velocityY, color);
+        this.alienType = alienType;
+        this.alienWidth = alienWidth;
+        this.alienHeight = alienHeight;
+        this.alienHealth = alienHealth;
+    }
+
+    public void draw(Graphics g) {
+        // Varient 1
+        if (this.alienType % 3 == 0) {
+            alien1.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
+            // Varient 2
+        } else if (this.alienType % 3 == 1 && this.alienType != 100) {
+            alien2.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
+            // Varient 3
+        } else if (this.alienType % 3 == 2) {
+            alien3.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
+            // Boss Enemy
+        } if (this.alienType == 100)
+        {
+            if(alienHealth > 20){
+                alienBoss.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
+            }
+            else if(alienHealth > 10){
+                alienBoss2.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
+            }
+            else if(alienHealth > 0){
+                alienBoss3.paintIcon(null, g, this.getXCoordinateValue(), this.getYCoordinateValue());
+            }
+        }
+    }
+
+    @Override
+    public Rectangle getBounds(){
+        Rectangle alienHitRectangle = new Rectangle(this.getXCoordinateValue(), this.getYCoordinateValue(), alienWidth, alienHeight);
+        return  alienHitRectangle;
+    }
+
+    @Override
+    public void move(){
+        this.setXCoordinateValue(this.getXCoordinateValue() + getVelocityX());
+    }
+
+    public int getAlienType() {
+        return alienType;
+    }
+
+    public int getAlienWidth() {
+        return alienWidth;
+    }
+
+    public int getAlienHeight() {
+        return alienHeight;
+    }
+
+    public int getAlienHealth() {
+        return alienHealth;
+    }
+
+    public void setAlienHealth(int alienHealth) {
+        this.alienHealth = alienHealth;
+    }
+}
+
---
+++ unittests/data/providers/HighscoreStoreTest.java
@@ -0,0 +1,45 @@
+package data.providers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HighscoreStoreTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPoints() {
+    }
+
+    @Test
+    void setPoints() {
+    }
+
+    @Test
+    void setHighscoreValue() {
+    }
+
+    @Test
+    void registerNewHighscore() {
+    }
+
+    @Test
+    void testRegisterNewHighscore() {
+    }
+
+    @Test
+    void getHighScore() {
+    }
+
+    @Test
+    void testGetHighScore() {
+    }
+}

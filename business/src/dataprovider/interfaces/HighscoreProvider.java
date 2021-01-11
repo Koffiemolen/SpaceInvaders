3f75150e0d547a49b36691ab091025e5f9cfadb4
Bastiaan Clement <bastiaan.clement@simac.com>
2021-01-11 20:43:10 +0100

    Added unittest for HighscoreStore.

--- business/src/dataprovider/interfaces/HighscoreProvider.java
+++ business/src/dataprovider/interfaces/HighscoreProvider.java
@@ -5,17 +5,11 @@ import java.io.File;
 public interface HighscoreProvider {
 
     String getName();
-
     int getPoints();
     void setPoints(int points);
-
     void setHighscoreValue(int score);
-
     void registerNewHighscore(File file, String name, int score);
-
     void registerNewHighscore(String name, int score);
-
     String GetHighScore(File file);
-
     String GetHighScore();
 }
